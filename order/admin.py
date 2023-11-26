fc01d2bcd989666b570ceb87b8f6e418bbbc3ba4
David Patrício <david.c.patricio@gmail.com>
2023-11-26 12:55:26 +0000

    Criar os models Order e OrderItem da app order

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class OrderItemInline(admin.TabularInline):
+    model = models.OrderItem
+    extra = 1
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [
+        OrderItemInline
+    ]
+
+
+admin.site.register(models.Order, OrderAdmin)
+admin.site.register(models.OrderItem)
--- project/settings.py
+++ project/settings.py
@@ -34,17 +34,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'order',
+    'product',
+    'profiles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    # APPS
-    'order',
-    'product',
-    'profiles',
 
     # TODO: Remover debug toolbar
     'debug_toolbar',
