3ff92d8de67e45b0e4835d412f1d042b4f74012c
David Patrício <david.c.patricio@gmail.com>
2023-11-26 13:07:49 +0000

    Criar o model Profile da app profiles

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,37 @@
-from django.db import models
+import re
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+from django.forms import ValidationError
+
+from utils.validate_nif import validate_nif
+
+
+class Profile(models.Model):
+    site_user = models.OneToOneField(
+        User, on_delete=models.CASCADE, verbose_name='User'
+    )
+    date_of_birth = models.DateField()
+    nif = models.CharField(max_length=9)
+    address = models.CharField(max_length=100)
+    number = models.CharField(max_length=10, blank=True)
+    complement = models.CharField(max_length=30, blank=True)
+    neighborhood = models.CharField(max_length=50, blank=True)
+    postal_code = models.CharField(max_length=8)
+    city = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.site_user}'
+
+    def clean(self):
+        error_messages = {}
+
+        if not validate_nif(self.nif):
+            error_messages['nif'] = 'Enter a valid NIF.'
+
+        if re.search(r'^\d{4}-\d{3}?$', self.postal_code) or \
+                len(self.postal_code) != 8:
+            error_messages['nif'] = 'Invalid postal code.'
+
+        if error_messages:
+            raise ValidationError(error_messages)
